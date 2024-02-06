e5f729cb3dab11c3cc18e646b00efed15ede7330
Angelina Ceppaluni <aceppaluni@gmail.com>
2024-02-06 12:19:01 -0500

    first commit

---
+++ express/index.js
@@ -0,0 +1,93 @@
+const cors = require('cors')
+const mongoose = require('mongoose');
+const express = require('express')
+const crypto = require('crypto')
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+const http = require('http').createServer(app)
+const io = require('socket.io')
+
+app.use(cors)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const jwt = require('jsonwebtoken');
+const Pup = require('./models/user');
+//const Chat = require('./models/chat')
+
+const url = 'mongodb://localhost:27017/pupsdb'
+mongoose.connect(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+.then(() => {
+    console.log('Connected to server')
+})
+.catch((error) => {
+    console.log('Error connecting to MongoDB', error)
+}); 
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
+
+
+// endpoint to register pup to backend 
+app.post('/register', async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        //check if pup is already registered
+        const existingPup = await Pup.findOne({email})
+        if(existingPup) {
+            return res.status(404).json({message: 'Pup already registered'})
+        }
+
+        //create a new pup
+        const newPup = new Pup({
+            name,
+            email,
+            password
+        })
+
+        //generate verification token for new pup
+        newPup.verificationToken = crypto.randomBytes(20).toString('hex')
+
+        //saving pup to DB
+        await newPup.save()
+
+        sendVerficationEmail(newPup.email, newPup.verificationToken);
+
+        res.status(200).json({message: 'Pup registered successfully', userId: newPup._id})
+        // may not be user may need to be pup
+    } catch (error) {
+        res.status(404).json({message: 'Registration failed'});
+    }
+});
+
+const sendVerficationEmail = async (email, verificationToken) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'aceppaluni@gmail.com',
+            pass: '#'
+        }
+    })
+
+    const mailOptions = {
+        from: 'furrypals.com',
+        to: email,
+        subject: 'Verification Email',
+        text: `Please use the link provided to verify your email : http://localhost:3000/verify/${verificationToken}`
+    }
+
+    try {
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.log('Error sending verification email')
+    }
+}
---
+++ express/models/user.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema 
+
+const Userschema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }, 
+    email: {
+        type: String,
+        required: true
+    }, 
+    password: {
+        type: String,
+        required: true
+    }, 
+    gender: {
+        type: String,
+        enum: ['male', 'female']
+    },
+    verified: {
+        type: Boolean,
+        required: true
+    },
+    verificationToken: String,
+    age: {
+        type: String,
+    },
+    crushes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Pup'
+        }
+    ],
+    recievedLikes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Pup'
+        }
+    ],
+    matches: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Pup'
+        }
+    ],
+    description: {
+        type: String,
+    },
+    profileImage: [
+        {
+            type: String
+        }
+    ],
+    lookingfor: [
+        {
+            type: String
+        }
+    ]
+})
+
+const Usermodel = mongoose.model('User', Userschema)
+module.exports = Usermodel
